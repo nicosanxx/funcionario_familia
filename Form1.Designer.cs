aa77b20a22ca73b1f923fbaa1e94b78c6c2cb663
leonardo.ocruz <leonardo.ocruz@PIR06305W10-1>
2022-05-27 16:21:41 -0300

    funcionario_familia

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace funcionario_familia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_hora = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_per_hr = new System.Windows.Forms.TextBox();
+            this.txt_kid = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(571, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Horas:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(93, 170);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 23);
+            this.txt_name.TabIndex = 5;
+            // 
+            // txt_hora
+            // 
+            this.txt_hora.Location = new System.Drawing.Point(93, 226);
+            this.txt_hora.Name = "txt_hora";
+            this.txt_hora.Size = new System.Drawing.Size(100, 23);
+            this.txt_hora.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Por Hora:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Filhos:";
+            // 
+            // txt_per_hr
+            // 
+            this.txt_per_hr.Location = new System.Drawing.Point(93, 283);
+            this.txt_per_hr.Name = "txt_per_hr";
+            this.txt_per_hr.Size = new System.Drawing.Size(100, 23);
+            this.txt_per_hr.TabIndex = 9;
+            // 
+            // txt_kid
+            // 
+            this.txt_kid.Location = new System.Drawing.Point(90, 336);
+            this.txt_kid.Name = "txt_kid";
+            this.txt_kid.Size = new System.Drawing.Size(100, 23);
+            this.txt_kid.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(289, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Resultado:";
+            // 
+            // txt_result
+            // 
+            this.txt_result.Location = new System.Drawing.Point(371, 345);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(100, 23);
+            this.txt_result.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 400);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_kid);
+            this.Controls.Add(this.txt_per_hr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_hora);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_hora;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_per_hr;
+        private System.Windows.Forms.TextBox txt_kid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace funcionario_familia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_hora.Text =="" || txt_kid.Text =="" || txt_per_hr.Text =="")
+            {
+
+                MessageBox.Show("FAVOR PREENCHER CORRETAMENTE OS CAMPOS", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            else
+            {
+                decimal horas = Convert.ToDecimal(txt_hora.Text);
+                decimal per_hr = Convert.ToDecimal(txt_per_hr.Text);
+                int filhos = Convert.ToInt32(txt_kid.Text);
+                decimal salario_bruto;
+                decimal salario_final;
+                decimal per_kid;
+
+                //CALCULOS
+                per_kid =(decimal)(filhos * 1.03);
+                salario_bruto = horas * per_hr;
+                salario_final = salario_bruto + per_kid;
+                txt_result.Text = salario_final.ToString();
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
